8760328a8c4dbf52f9fe45e8c6e3540d40b04f3e
alexandre steinhauslin <a.steinhauslin@gmail.com>
2020-09-11 18:06:24 -0700

    login complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
+    <div className="App">
+        <div className="navigation">
+          <Link to="/Login" className="link">Login</Link>
+          <Link to="/protected" className="link">Bubble Page</Link>
+        </div>
+      <Switch>
+        <PrivateRoute exact path="/protected" component={BubblePage} />
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
-      </div>
-    </Router>
+        <Route component={Login}/>
+      </Switch>
+    </div>
   );
 }
 
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,68 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = (props) => {
+  
+const initialState = {
+  credentials: {
+  username: "",
+  password: ""
+}};
+
+const [formState, setFormState] = useState(initialState);
+//const {push} = useHistory();
+
+const userLogin = e => {
+  e.preventDefault();
+  console.log("logging in")
+  axios
+    .post("http://localhost:5000/api/login", formState.credentials)
+    .then(res => {
+      localStorage.setItem("token", res.data.payload);
+      props.history.push('/protected');
+      console.log("token returned");
+    })
+    .catch(err=>console.log("login error:", err))
+};
+
+const handleChanges = e => {
+  e.persist();
+  //console.log("input change:", e.target.name, e.target.value);
+  setFormState({
+    credentials: {...formState.credentials, 
+    [e.target.name]: e.target.value
+  }});
+};
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <p>Please Login</p>
+      <form onSubmit={userLogin}>
+        <label>
+          Username:
+          <input
+            type = "text"
+            id =  "username"
+            name = "username"
+            placeholder = "enter username"
+            value = {formState.credentials.username}
+            onChange = {handleChanges}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type = "text"
+            id =  "password"
+            name = "password"
+            placeholder = "enter password"
+            value = {formState.credentials.password}
+            onChange = {handleChanges}
+          />
+        </label>
+        <button>Log In</button>
+      </form>
     </>
   );
 };
